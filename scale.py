ab6ad003c93be6069a4c6eef8269f5f2a38c6d01
NguyenDuck <duck.duc.vit@gmail.com>
2021-07-11 12:15:15 +0700

    Change for easy way understand

--- scale.py
+++ scale.py
@@ -1,23 +1,35 @@
+
 import json
+
 print('Thankyou for using my scale generator.')
-res = int(input("Please enter your resolution. For example, if you're using a 128x128 resolution, only input 128! "))
 
-## Equation 1= 0.075/(texture_resolution/16)
-## Equation 2= 0.125/(texture_resolution/16)
+RESOLUTION = int(input("Please enter your resolution. For example, if you're using a 128x128 resolution, only input 128! "))
 
-val1=0.075
-val2=0.125
+## x, z = 0.075/(texture_resolution/16)
+## y    = 0.125/(texture_resolution/16)
 
-## Scale 1 value in offset
-scale_x = val1/(res / 16)
+values = [0.075, 0.125]
 
-## Scale 2 value in offset
-scale_y = val2/(res/16)
-
-## Scale 3 value in offset
-scale_z = val1/(res / 16)
+## Scale values in offset
+scales = [
+	values[0]/(RESOLUTION/16),
+	values[1]/(RESOLUTION/16),
+	values[0]/(RESOLUTION/16)
+]
 
 ##Print offset component with scale values
-data = {'minecraft:render_offsets': {'main_hand': {'first_person': {'scale': [scale_x,scale_y,scale_z]},'third_person': {'scale': [scale_x,scale_y,scale_z]}},'off_hand': {'first_person': {'scale': [scale_x,scale_y,scale_z]},'third_person': {'scale': [scale_x,scale_y,scale_z]}}}}
+data = {
+	'minecraft:render_offsets': {
+		'main_hand': {
+			'first_person': {'scale': scales},
+			'third_person': {'scale': scales}
+		},
+		'off_hand': {
+			'first_person': {'scale': scales},
+			'third_person': {'scale': scales}
+		}
+	}
+}
 jstr = json.dumps(data, indent=4)
+
 print(jstr)
